208cf59b90ca506dfd605c42b07ec774e7421a16
adinath <adinathuparkar23@gmail.com>
2024-12-01 18:30:45 +0530

    feat: :construction: test auto update changes

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-
+import autoUpdate from "./lib/autoupdate.ts";
+autoUpdate();
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
